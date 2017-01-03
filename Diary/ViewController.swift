da0adc43c21a1d56b234df00255e30f5a3d25025
almond <dhkim20157@gmail.com>
2017-01-03 13:34:18 +0900

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by almond on 2017. 1. 3..
+//  Copyright © 2017년 almond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
