8047e99740d000829662f8b360713f51d90ef8ce
almond <dhkim20157@gmail.com>
2017-01-04 16:34:54 +0900

    텍스트필드 추가

--- Diary/AddPostViewController.swift
+++ Diary/AddPostViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class AddPostViewController: UIViewController {
     
     let cancelButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: nil, action: nil)
+    let titleField = UITextField()
+    let contentField = UITextView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +20,33 @@ class AddPostViewController: UIViewController {
         self.view.backgroundColor = .white
         self.title = "글쓰기"
         
+        self.view.addSubview(titleField)
+        self.view.addSubview(contentField)
+        
+        self.titleField.borderStyle = UITextBorderStyle.roundedRect
+        
         self.cancelButtonItem.target = self
         self.cancelButtonItem.action = #selector(cancelButtonDidSelected)
         
         self.navigationItem.leftBarButtonItem = cancelButtonItem
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.titleField.frame = CGRect(
+            x: 10,
+            y: 70,
+            width: self.view.frame.size.width - 20,
+            height: 44
+        )
+        
+        self.contentField.frame = CGRect(
+            x: 10,
+            y: 124,
+            width: self.view.frame.size.width - 20,
+            height: 100
+        )
     }
     
     func cancelButtonDidSelected() {
